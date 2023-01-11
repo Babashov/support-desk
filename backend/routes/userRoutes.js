9f92696af8d664a9230a465f8af942489ba292ae
Babashov Mahammad <babashov.info@gmail.com>
2023-01-12 03:33:49 +0400

    backend userController errorMiddleware userRoutes & express-async-handler package installed

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const route = express.Router()
+const {registerUser,loginUser} = require('../controllers/userController')
+
+route.post('/',registerUser)
+route.post('/login',loginUser)
+
+
+module.exports = route
