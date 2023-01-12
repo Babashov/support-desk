c74e9e71b84c76f35c931426724aa096547d20f6
Babashov Mahammad <babashov.info@gmail.com>
2023-01-13 00:59:48 +0400

    added " /me" route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const route = express.Router()
-const {registerUser,loginUser} = require('../controllers/userController')
+const {registerUser,loginUser,getMe} = require('../controllers/userController')
+const {protect} =  require('../middleware/authMiddleware')
 
 route.post('/',registerUser)
 route.post('/login',loginUser)
-
+route.get('/me',protect,getMe)
 
 module.exports = route
