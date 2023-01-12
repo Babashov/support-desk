43dca0d586da928721e7c33cf6dea12fcc097055
Babashov Mahammad <babashov.info@gmail.com>
2023-01-12 22:33:37 +0400

    database connection

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+// It's optional to turn off warning in console
+mongoose.set('strictQuery', true);
+
+const connectDb = async()=>{
+
+    try {
+        
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDb connected: ${conn.connection.host}`.cyan.underline);
+
+    } catch (error) {
+        console.log(`Error: ${error.message}`.red.underline.bold);
+        process.exit(1)
+    }
+
+}
+
+module.exports = connectDb
