d7c28bb14997f86b3c1428a2372c235730023ccb
Babashov Mahammad <babashov.info@gmail.com>
2023-01-13 22:20:15 +0400

    Iphone changed iPhone in enum

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -11,7 +11,7 @@ const ticketSchema = mongoose.Schema({
     product:{
         type: String,
         required:[true,'Please select product'],
-        enum:['Iphone','Macbook Pro','iMac','iPad']
+        enum:['iPhone','Macbook Pro','iMac','iPad']
     },
 
     description:{
