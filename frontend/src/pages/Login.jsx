f9bbe12ccee04d9dc6df6444396cdc8c00355617
Babashov Mahammad <babashov.info@gmail.com>
2023-01-13 15:18:17 +0400

    isError & useEffect added for check login status

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,39 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import {FaSignInAlt} from "react-icons/fa"
 import {toast} from 'react-toastify'
 import {useSelector,useDispatch} from 'react-redux'
-import { login } from "../features/auth/authSlice"
+import { useNavigate } from "react-router-dom"
+import { login,reset } from "../features/auth/authSlice"
 
 function Login() {
   const [formData,setFormData] = useState({
     email:'',
     password:'',
+    isError:false
   })
   const {email,password} = formData
 
   const dispatch = useDispatch()
 
-  const {user,isLoading,isSuccess,message} = useSelector(state=>state.auth)
+  const {user,isLoading,isError,isSuccess,message} = useSelector(state=>state.auth)
+
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+
+    if(isError)
+    {
+      toast.error(message)
+    }
+
+    if(isSuccess || user)
+    {
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  },[user,isSuccess,message,isError,dispatch,navigate])
 
 
   const onChange = (e)=>{
