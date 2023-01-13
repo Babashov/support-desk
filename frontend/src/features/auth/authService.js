4d9bad3425a5c29f7da756a2506f42ccc86da1ab
Babashov Mahammad <babashov.info@gmail.com>
2023-01-13 15:16:36 +0400

    added login & logout method

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -14,8 +14,29 @@ const register = async (userData)=>{
     return response.data
 }
 
+// Logout
+const logout = () => localStorage.removeItem('user')
+
+
+// Login
+const login = async(userData)=>{
+
+    const response = await axios.post(API_URL + '/login',userData)
+
+    if(response.data)
+    {
+        localStorage.setItem('user',JSON.stringify(response.data))
+    }
+
+    return response.data
+
+}
+
+
 const authService = {
-    register
+    register,
+    logout,
+    login
 }
 
 export default authService
