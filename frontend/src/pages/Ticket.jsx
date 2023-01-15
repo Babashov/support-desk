e80af64c3c3ac82f87184d10057b5245507a219c
Babashov Mahammad <babashov.info@gmail.com>
2023-01-15 14:23:01 +0400

    onNoteSubmit() is ready

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -2,7 +2,7 @@ import { useEffect,useState } from "react"
 import {useSelector,useDispatch} from 'react-redux'
 import { useParams,useNavigate } from "react-router-dom"
 import { getTicket,closeTicket } from "../features/tickets/ticketSlice"
-import {getNotes,reset as notesReset} from "../features/notes/noteSlice"
+import {getNotes,createNote, reset as notesReset} from "../features/notes/noteSlice"
 import Spinner from "../components/Spinner"
 import { BackButton } from "../components/BackButton"
 import {toast} from 'react-toastify'
@@ -53,7 +53,7 @@ function Ticket() {
     // Create note submit
     const onNoteSubmit = (e)=>{
         e.preventDefault()
-        console.log('on submit');
+        dispatch(createNote({noteText,ticketId}))
         closeModal()
     }
 
